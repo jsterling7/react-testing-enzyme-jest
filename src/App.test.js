93594381ce4e325c93782ffabb892884e8f9f1cc
Alex Hall <james_a_hall@homedepot.com>
2018-10-23 14:23:33 -0400

    Remove default create-react-app boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('sanity testing the test suite', () => {
+    it('works!', () => {
+        expect(1).toEqual(1)
+    })
+})
+    })
+})
+
