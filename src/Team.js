d6fd49dc1fe332284c87b5d9b9efffd96324927b
Alex Hall <james_a_hall@homedepot.com>
2018-11-03 14:11:36 -0400

    Corresponding source code for Team

--- src/Team.js
+++ src/Team.js
@@ -1,7 +1,46 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class Team extends Component {
-    render(){
-        return(<div>TEAMS!</div>)
+
+    state = {
+        playerData: []
     }
+
+    async componentDidMount() {
+        const {data} = await axios.get('http://super_cool_site.biz/players')
+
+
+        this.setState({
+            playerData: data
+        })
+    }
+
+    renderPlayers = playerData => {
+        return (
+            playerData.map(player => (
+                    <div
+                        data-player-name
+                        key={player.name}>
+                        {player.name}
+                    </div>
+                )
+            )
+        )
+    }
+
+    render() {
+        const {playerData} = this.state
+
+        if (playerData) {
+            return (
+                <div>
+                    {this.renderPlayers(playerData)}
+                </div>
+            )
+        } else {
+            return (<div>LOADING!!!!</div>)
+        }
+    }
+
 }
