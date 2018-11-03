a3236ce44c04823c4ac6f111b2c4ca81e52170ef
Alex Hall <james_a_hall@homedepot.com>
2018-11-03 12:45:16 -0400

    Test driving out the team search component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,85 @@ import React, {Component} from 'react'
 import './App.css'
 
 class App extends Component {
+    state = {
+        searchTerm: '',
+        searchResult: null,
+        teams: [
+            {
+                id: 1,
+                name: "Birmingham Bolts",
+                location: "Birmingham, Alabama",
+                colors: "Purple, Yellow, Silver"
+            }, {
+                id: 2,
+                name: "Orlando Rage",
+                location: "Orlando, Florida",
+                colors: "Red, Navy, Gold, White"
+            }, {
+                id: 3,
+                name: "Seattle Street Sharks",
+                location: "Seattle, Washington",
+                colors: "Red, Gray, Blue, Black"
+            }
+        ]
+    }
+
+
+    updateTeamSearchInput = ({target: {value}}) => {
+        this.setState({searchTerm: value})
+    }
+
+    searchForTeam = (e) => {
+        e.preventDefault()
+
+        let {teams, searchTerm} = this.state
+
+        const searchResult = teams.filter(team => team.name === searchTerm)[0]
+
+        this.setState({
+            searchResult
+        })
+    }
+
+    renderSearchResults() {
+        let {searchResult} = this.state
+
+        if (searchResult) {
+            return (
+                <div data-team-search-results>
+                    {searchResult.name} - {searchResult.location} - Colors: {searchResult.colors}
+                </div>
+            )
+        } else {
+            return (
+                <div/>
+            )
+        }
+    }
+
     render() {
+
+
         return (
             <div className="App">
                 <h1>Welcome to the Fantasy Football App!</h1>
                 <h3>The central source for all drafting, roster moves, and the latest player information.</h3>
+
+                <div>
+                    <input
+                        type="text"
+                        onChange={this.updateTeamSearchInput}
+                        data-team-search-input
+                    />
+                    <button
+                        onClick={this.searchForTeam}
+                        data-team-search-submit
+                    >
+                        Search!
+                    </button>
+
+                    {this.renderSearchResults()}
+                </div>
             </div>
         )
     }
--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,7 @@ describe('sanity testing the test suite', () => {
     })
 })
 
-describe("Landing Page", () => {
+describe("Landing Page static content", () => {
     let component
 
     beforeEach(() => {
@@ -27,5 +27,34 @@ describe("Landing Page", () => {
 
         expect(welcomeElement.exists()).toEqual(true)
         expect(welcomeElement.text()).toEqual('The central source for all drafting, roster moves, and the latest player information.')
+    })
+})
+
+describe("Searching for teams", () => {
+    describe("when the team is found", () => {
+        it("displays additional information about the teams", () => {
+            const component = shallow(<App/>)
+
+            const event = {
+                target: {
+                    value: "Birmingham Bolts"
+                }
+            }
+
+            component
+                .find("[data-team-search-input]")
+                .simulate("change", event)
+
+            component
+                .find("[data-team-search-submit]")
+                .simulate("click", { preventDefault: () => {}})
+
+            const searchResults = component
+                .find('[data-team-search-results]')
+                .text();
+
+            expect(searchResults).toEqual("Birmingham Bolts - Birmingham, Alabama - Colors: Purple, Yellow, Silver")
+
+        })
     })
 })
