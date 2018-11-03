a70493e2bb70911241ddd4d2448a459b1b64ccf0
Alex Hall <james_a_hall@homedepot.com>
2018-11-03 13:20:32 -0400

    Add Tests to drive out the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import LandingPage from "./LandingPage";
+
 
 export class App extends Component {
 
     render(){
         return(
             <div>
-
+                <Router>
+                    <Switch>
+                        <Route path={"/"} component={LandingPage}/>
+                    </Switch>
+                </Router>
             </div>
         )
     }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {App} from './App'
 import {shallow} from 'enzyme'
+import {Route} from "react-router-dom";
+import LandingPage from "./LandingPage";
 
 describe('App router functionality', () => {
     it('renders the laning page by default', () => {
         const app = shallow(<App/>)
         expect(app.exists()).toBeTruthy()
+    })
+
+    it('routes to the landing component by default', () =>  {
+        const app = shallow(<App/>)
+
+        const defaultRoute = app.find(Route)
+
+        expect(defaultRoute.props().path).toEqual("/")
+        expect(defaultRoute.props().component).toEqual(LandingPage)
     })
 })
