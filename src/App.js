c5d5d3593fe61dced161174532db0dc28a46572b
Alex Hall <james_a_hall@homedepot.com>
2018-11-05 14:28:29 -0500

    Add bonus to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, {Component} from 'react'
 import './App.css'
 
 class App extends Component {
+    state = {
+        playerList: [{
+            name: "Usain Bolt"
+        }, {
+            name: "Bart Simpson"
+        }]
+    }
+
+    renderTeamList() {
+        return (
+            <ul>
+                {
+                    this.state.playerList.map(({name}) => (
+                        <li
+                            className='make-bold'
+                            key={name}
+                        >{name}</li>
+                    ))
+                }
+            </ul>
+        )
+    }
+
     render() {
         return (
             <div className="App">
-
-
+                {this.renderTeamList()}
             </div>
         )
     }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import App from './App'
+import {shallow} from "enzyme";
 
 describe('sanity testing the test suite', () => {
     it('should work', () => {
-        expect(1).toEqual(2)
+        expect(1).toEqual(1)
     })
 })
 
+describe('List of additional players', () => {
+
+    it('displays a list of players', () => {
+        const component = shallow(<App/>)
+
+        let playerList = component
+            .find('ul > li');
+
+        let firstPlayer = playerList.at(0);
+
+        expect(firstPlayer.exists()).toBeTruthy()
+
+        expect(firstPlayer.text()).toEqual("Usain Bolt")
+
+        let secondPlayer = playerList.at(1);
+
+        expect(secondPlayer.text()).toEqual("Bart Simpson")
+
+        expect(playerList.at(0).props().className).toEqual("make-bold")
+    })
+
+
+})
