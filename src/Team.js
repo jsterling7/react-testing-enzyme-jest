f3552001a354e09c58caef972c773cab5cc8e6f0
Alex Hall <james_a_hall@homedepot.com>
2018-11-16 11:54:10 -0500

    Quick push up

--- src/Team.js
+++ src/Team.js
@@ -13,19 +13,39 @@ export default class Team extends Component {
     }
 
     async componentDidMount() {
-        const response = await axios.get('http://some_cool_service.biz/birminghambolts/players')
 
-        this.setState({
-            playerData: response.data
-        })
+        try {
+            const response = await axios.get('http://some_cool_service.biz/birminghambolts/players')
+
+            this.setState({
+                playerData: response.data
+            })
+
+        } catch (e) {
+            this.setState({
+                errorMessage: e.data
+            })
+        }
+    }
+
+    renderPlayerData = () => {
+        if (this.state.playerData.length) {
+            return (
+                <div data-team-players>
+                    {this.state.playerData.map(player => <div key={player.name}>{player.name} - {player.team}</div>)}
+                </div>
+            )
+        } else {
+            return (
+                <div data-error-message>{this.state.errorMessage}</div>
+            )
+        }
     }
 
     render() {
         return (
             <div>
-                <div data-team-players>
-                    {this.state.playerData.map(player => <div key={player.name}>{player.name} - {player.team}</div>)}
-                </div>
+                {this.renderPlayerData()}
             </div>
         )
     }
--- src/Team.test.js
+++ src/Team.test.js
@@ -5,18 +5,18 @@ import axios from 'axios'
 
 jest.mock('axios', () => ({
     get: () => {
-        throw 'YOU SHOULD MOCK ME'
+        throw 'YOU SHOULD IMPLEMENT A REAL MOCK'
     }
 }))
 
 describe("When visiting the teams page", () => {
-    describe("When able to successfully get player data", () => {
+    describe("When able to fetch player data", () => {
         beforeEach(() => {
             const dataToReturn = {
                 data: [
                     {
                         name: 'Usain Bolt',
-                        team : 'Birmingham Bolts'
+                        team: 'Birmingham Bolts'
                     }
                 ]
             };
@@ -32,11 +32,36 @@ describe("When visiting the teams page", () => {
             await axios.get()
 
             const players = component.find('[data-team-players]')
+            const errorComponent = component.find('[data-error-message]');
 
             let firstPlayer = players.at(0);
 
             expect(firstPlayer.text()).toEqual("Usain Bolt - Birmingham Bolts")
 
+            expect(errorComponent.text()).not.toEqual('Failed to load data!')
+
+        })
+    })
+
+    describe("When unable to fetch any player data", () => {
+        it("Should display a warning message", async () => {
+            let expectedErrorMessage = "Failed to load data!";
+
+            axios.get = jest.fn(() => {
+                return Promise.reject({data: expectedErrorMessage})
+            })
+
+            const component = shallow(<Team/>)
+
+            try {
+                await axios.get()
+            } catch (e) {
+
+            }
+
+            const errorComponent = component.find('[data-error-message]')
+
+            expect(errorComponent.text()).toEqual(expectedErrorMessage)
         })
     })
 })
